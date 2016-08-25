fbaadd4d09ca83073ac037cc7175bae8d3d86343
Shintaro Kojima <goodies@codeout.net>
2016-08-25 23:56:29 +0900

    Refactoring

--- lib/hosty/hosts.rb
+++ lib/hosty/hosts.rb
@@ -13,19 +13,34 @@ module Hosty
       private
 
       def parse(str, &block)
-        content(str).each_line do |l|
-          columns = l.split
-          next unless columns[0].local_address?
-
-          if columns.last.port_number?
-            yield servers: columns[1..-2], port: columns.last.port_number
-          end
-        end
+        str.each_line {|l| new(l).parse &block }
       end
+    end
 
-      def content(raw)
-        raw.skip_comment_lines.shorten_port_definitions
-      end
+
+    def initialize(line)
+      @line = line
+    end
+
+    def parse(&block)
+      return unless content
+      yield content
+    end
+
+
+    private
+
+    def content
+      return if @content
+
+      columns = @line.split
+      return unless columns[0].local_address?
+
+      mark_index = columns.index('#')
+      return unless mark_index
+      return unless columns[mark_index+1] && columns[mark_index+1].port_number?
+
+      {servers: columns[1..mark_index-1], port: columns[mark_index+1].port_number}
     end
   end
 end
--- lib/portable_string.rb
+++ lib/portable_string.rb
@@ -4,14 +4,6 @@ module PortableString
       self == '127.0.0.1' || self == '::1'
     end
 
-    def skip_comment_lines
-      gsub(/^ *#.*\n/, '')
-    end
-
-    def shorten_port_definitions
-      gsub(/# *(:\d+).*/) { $1 }
-    end
-
     def port_number?
       port_number != nil
     end
