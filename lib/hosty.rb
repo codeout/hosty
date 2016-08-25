4bc201788a6ad6e1496b9e80cc489cc29c2f46ed
Shintaro Kojima <goodies@codeout.net>
2016-08-25 23:26:57 +0900

    Remove unused module

--- lib/hosty.rb
+++ lib/hosty.rb
@@ -1,7 +1,5 @@
 require 'hosty/hosts'
-require 'hosty/scheme_detectable'
 require 'hosty/virtual_hostable'
 require 'rack/reverse_proxy'
 
 RackReverseProxy::RoundTrip.prepend Hosty::VirtualHostable
-RackReverseProxy::Rule::Candidate.prepend Hosty::SchemeDetectable
