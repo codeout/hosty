01b88659638d3e42e1c4077997cb6e96ae865e02
Shintaro Kojima <goodies@codeout.net>
2018-01-17 20:55:10 +0900

    Introduce debug mode

--- lib/hosty/virtual_hostable.rb
+++ lib/hosty/virtual_hostable.rb
@@ -1,7 +1,30 @@
 module Hosty
   module VirtualHostable
+    def proxy
+      debug "<= #{env['REQUEST_METHOD']} #{source_request.url}"
+
+      return app.call(env) if uri.nil?
+      return https_redirect if need_https_redirect?
+
+      setup_request
+      setup_response_headers
+
+      debug "  -> #{env['REQUEST_METHOD']} #{uri}"
+
+      transform_response(rack_response).tap {|t|
+        debug "  <- #{t[0]}"
+      }
+    end
+
     def rules
       super.select {|r| r.options[:vhost] == env['SERVER_NAME'] }
+    end
+
+
+    private
+
+    def debug(message)
+      puts "#{Time.now} #{message}" if $HOSTY_DEBUG
     end
   end
 end
